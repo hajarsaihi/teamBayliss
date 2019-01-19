9eab70922afeaa97302f9998b2272ffa846660e2
Omer Baskan <37221382+omerbaskan@users.noreply.github.com>
2019-01-19 18:45:54 +0000

    Add files via upload

---
+++ Data_Collection/phosphosite_data/kinase_only.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+df= pd.read_csv("data.csv")     # Reading csv file 
+data = open("gene_list.txt").read().split("\n")  # Reading gene list 
+selected = df[df.GENE.isin((data))]  # Only data which is the correct gene
+selected = selected[selected.ORGANISM=='human']  # Only data which has the organism as human
+selected.to_csv("output.csv")
+print("The output.csv is generated. ")
---
+++ Data_Collection/phosphosite_data/kinase_substate.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+df = pd.read_csv("dataset\kinase_substrate_dataset.csv")     # Reading csv file 
+selected = df[(df.KIN_ORGANISM=='human') & (df.SUB_ORGANISM=='human')] # Only data which has the kin_organism and sub_organism as human
+selected.to_csv("kinase_substrate.csv")
+print("The kinase_substrate.csv is generated. ")
