dd514f7a8d3f0c9977b59def35315e1f4d68b618
Omer Baskan <37221382+omerbaskan@users.noreply.github.com>
2019-01-19 20:36:05 +0000

    Add files via upload

--- Data_Collection/phosphosite_data/kinase_only.py
+++ Data_Collection/phosphosite_data/kinase_only.py
@@ -4,5 +4,5 @@ df= pd.read_csv("data.csv")     # Reading csv file
 data = open("gene_list.txt").read().split("\n")  # Reading gene list 
 selected = df[df.GENE.isin((data))]  # Only data which is the correct gene
 selected = selected[selected.ORGANISM=='human']  # Only data which has the organism as human
-selected.to_csv("output.csv")
+selected.to_csv("output.csv", index = False)
 print("The output.csv is generated. ")
