30c089c00807b698e8a1df173d551c5bff8316fd
Hajar <Hajar@0s-MacBook-Pro.broadband>
2019-01-21 20:48:45 +0000

    update models.py

--- Web1/models.py
+++ Web1/models.py
@@ -1,20 +1,23 @@
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///final_data.db'
+db = SQLAlchemy(app)
 
-db = SQLAlchemy()
+class Kinase_Information(db.Model):
+    __tablename__ = 'Kinase_Information'
+    kinase = db.Column('Kinase_Name', db.Unicode, primary_key = True)
+    family = db.Column('Family', db.Unicode)
+    subFamily = db.Column('SubFamily', db.Unicode)
+    entrez = db.Column('ENTREZ_GENEID', db.Unicode)
+    location = db.Column('Location', db.Unicode)
 
-class Protein_Kinase(db.Model):
-  uid = db.Column(db.Integer, primary_key = True)
-
-
- # def __init__(self, firstname, lastname, email, password):
-    #self.firstname = firstname.title()
-   # self.lastname = lastname.title()
-    #self.email = email.lower()
-    #self.set_password(password)
-     
-  #def set_password(self, password):
-    #self.pwdhash = generate_password_hash(password)
-
-  #def check_password(self, password):
-    #return check_password_hash(self.pwdhash, password)
+class Phosphosites(db.Model):
+    __tablename__ = 'Kinase_Phosphosite'
+    Phosphosite_ID = db.Column('Kinase_Name', db.Integer, primary_key = True)
+    gene = db.Column('Family', db.Unicode)
+    chr_loc = db.Column('HU_CHR_LOC', db.Unicode)
+    mod_res = db.Column('MOD_RSD', db.Unicode)
+    accession = db.Column('ACC_ID', db.Unicode)
+    site_seq = db.Column('SITE_SEQ', db.Unicode)
