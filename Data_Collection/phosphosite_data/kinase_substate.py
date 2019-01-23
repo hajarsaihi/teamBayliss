40f7bc9a347a9a1d4413c1b0e7657858f4fa9538
Omer Baskan <37221382+omerbaskan@users.noreply.github.com>
2019-01-23 15:49:52 +0000

    Add files via upload

---
+++ Data_Collection/phosphosite_data/kinase_substate.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+df = pd.read_csv("dataset/kinase_substrate_dataset.csv")     # Reading csv file 
+selected = df[(df.KIN_ORGANISM=='human') & (df.SUB_ORGANISM=='human')] # Only data which has the kin_organism and sub_organism as human
+selected = selected.drop(columns=['KIN_ORGANISM', 'SUB_ORGANISM']) # Drops the columns we don't want
+selected.to_csv("kinase_substrate.csv", index = False)
+print("The kinase_substrate.csv is generated. ")
