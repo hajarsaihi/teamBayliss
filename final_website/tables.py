3475423bfd38f6e7666bed8a425fd8f8310ae46e
startswithH <hajarsaihi@live.co.uk>
2019-01-23 21:22:08 +0000

    update inhib and phospho

--- final_website/tables.py
+++ final_website/tables.py
@@ -15,8 +15,10 @@ class IResults(Table):
     type = Col('Type')
 
 class PResults(Table):
-    chembl_ID = Col('ChEMBL_ID')
-    name = Col('Name')
-    syn = Col('Synonyms')
-    mol_formula = Col('Molecular_Formula')
-    type = Col('Type')
+    gene = Col('GENE')
+    pkinase = Col('KINASE')
+    kinase_accession = Col('KIN_ACC_ID')
+    substrate_protein= Col('SUBSTRATE')
+    sub_gene = Col('SUB_GENE')
+    sub_accession = Col('SUB_ACC_ID')
+    Phosphosite = Col('SITE_7_AA')
